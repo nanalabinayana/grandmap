c5f3e2aeb648c457f93dfe7a80b346f16ebbed5c
nanalabinayana <nanalabinayana@gmail.com>
2025-09-07 12:08:18 +0700

    Update LGMCakra-origin.js

--- LGMCakra-origin.js
+++ LGMCakra-origin.js
@@ -189,18 +189,21 @@ const PAGES = [
     hdr:{ x:40, maxWidth: 310 } },
 ]},
   { name:"Study Plan 1–4", bg:FRAME[2], boxes:[
-    { id:'p2a', title:'1st Term', x:83,  y:365, w:435, h:400, preset:'BIG', text:'' },
-    { id:'p2b', title:'2nd Term', x:563, y:363.22, w:435.89, h:401.78, preset:'BIG', text:'' },
-    { id:'p2c', title:'3rd Term', x:83,  y:804.19, w:435, h:400, preset:'BIG', text:'' },
-    { id:'p2d', title:'4th Term', x:563, y:805, w:435, h:400.81, preset:'BIG', text:'' },
-  ]},
-  { name:"Study Plan 5–8", bg:FRAME[3], boxes:[
-    { id:'p3a', title:'5th Term', x:83,  y:365, w:435, h:400, preset:'BIG', text:'' },
-    { id:'p3b', title:'6th Term', x:563, y:363.22, w:435.89, h:401.78, preset:'BIG', text:'' },
-    { id:'p3c', title:'7th Term', x:83,  y:804.19, w:435, h:400, preset:'BIG', text:'' },
-    { id:'p3d', title:'8th Term', x:563, y:805, w:435, h:400.81, preset:'BIGL', text:'' },
-  ]},
-
+  { id:'p2a', title:'1st Term', x:83,  y:365, w:435, h:400, preset:'BIG', text:'',
+    hdr:{ x:80, y:28, maxWidth:350 } },   // header digeser kanan
+  { id:'p2b', title:'2nd Term', x:563, y:363.22, w:435.89, h:401.78, preset:'BIG', text:'' },
+  { id:'p2c', title:'3rd Term', x:83,  y:804.19, w:435, h:400, preset:'BIG', text:'',
+    hdr:{ x:80, y:28, maxWidth:350 } },   // header digeser kanan
+  { id:'p2d', title:'4th Term', x:563, y:805, w:435, h:400.81, preset:'BIG', text:'' }
+]},
+{ name:"Study Plan 5–8", bg:FRAME[3], boxes:[
+  { id:'p3a', title:'5th Term', x:83,  y:365, w:435, h:400, preset:'BIG', text:'',
+    hdr:{ x:80, y:28, maxWidth:350 } },
+  { id:'p3b', title:'6th Term', x:563, y:363.22, w:435.89, h:401.78, preset:'BIG', text:'' },
+  { id:'p3c', title:'7th Term', x:83,  y:804.19, w:435, h:400, preset:'BIG', text:'',
+    hdr:{ x:80, y:28, maxWidth:350 } },
+  { id:'p3d', title:'8th Term', x:563, y:805, w:435, h:400.81, preset:'BIG', text:'' }
+]},
 
   { name:"Life Grand Map 2025–2035", bg:FRAME[4], boxes:[
     { id:'p4a', title:'2025 – 2030', x:91,  y:438, w:430, h:489, preset:'BIG', text:'',
@@ -483,6 +486,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
   buildPager();
   renderPage(0);
 });
+
 
 
 
