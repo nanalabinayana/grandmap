f0cae1337b7ba253b2a80b9a170614985ebe4233
nanalabinayana <nanalabinayana@gmail.com>
2025-09-07 11:52:00 +0800

    Update LGMCakra-origin.js

--- LGMCakra-origin.js
+++ LGMCakra-origin.js
@@ -186,9 +186,20 @@ const PAGES = [
     { id:'p2d', title:'8th Term', x:563, y:805, w:435, h:400.81, preset:'BIGL', text:'' },
   ]},
 
-  
+
+  { name:"Life Grand Map 2025–2035", bg:FRAME[4], boxes:[
+    { id:'p4a', title:'2025 – 2030', x:91,  y:438, w:430, h:489, preset:'BIG', text:'',
+
+      hdr: { abs: true, x: 200, y: 454, size: 35, weight: 280, color: "#FFFFFF", maxWidthAbs: 430, align:"center"},
+      txt: { abs: true, x: 130, y: 454 + 48 + 20, size: 25, weight: 360, color: "#2B2B2B", maxWidthAbs: 360, lineHeight: 1.45, letterSpacing: 0}
+     },
+    { id:'p4b', title:'2030 – 2035', x:580, y:438, w:430, h:489, preset:'BIG', text:'',
+       hdr: { abs: true, x: 715, y: 454, size: 35, weight: 280, color: "#FFFFFF", maxWidthAbs: 430, align:"center"},
+       txt: { abs: true, x: 610, y: 537, size: 25, weight: 360, color: "#2B2B2B", maxWidthAbs: 360, lineHeight: 1.45, letterSpacing: 0}
+     },
+  ]},
   { name:"Life Grand Map 2035–Beyond", bg:FRAME[5], boxes:[
-    { id:'p5a', title:'2035 – 2040', x:91,  y:332.73, w:430, h:613.82, preset:'SMALL', text:'',
+    { id:'p5a', title:'2035 – 2040', x:91,  y:332.73, w:430, h:613.82, preset:'BIG', text:'',
        hdr: { abs: true, x: 200, y: 454, size: 35, weight: 280, color: "#FFFFFF", maxWidthAbs: 430, align:"center"},
        txt: { abs: true, x: 125, y: 537, size: 25, weight: 360, color: "#2B2B2B", maxWidthAbs: 360, lineHeight: 1.45, letterSpacing: 0}
     },
@@ -197,23 +208,24 @@ const PAGES = [
        txt: { abs: true, x: 610, y: 537, size: 25, weight: 360, color: "#2B2B2B", maxWidthAbs: 360, lineHeight: 1.45, letterSpacing: 0}
      },
   ]},
-  
   { name:"Community Service & Grand Goals", bg:FRAME[6], boxes:[
     {
       id:'p6a', title:'Contribution Plan',
       // BG PNG (absolut ke kanvas)
-      x:72, y:359, w:926, h:381, preset:'BIG', text:'',
+      x:83, y:375, w:915, h:365, preset:'BIG', text:'',
       // TEKS absolut ke kanvas (tidak ikut BG)
-      hdr:{ abs:true, x:183, y:391, size:40, weight:700, color:'#FFFFFF', maxWidthAbs:457 },
-      txt:{ abs:true, x:121, y:474, size:25, weight:400, color:'#1E1E1E', maxWidthAbs:840 }
+      
+      hdr: { abs:true, x:183, y:400, size:40, weight:457, color: "#FFFFFF", maxWidthAbs: 457, align:"center"},
+      txt: { abs: true, x: 121, y: 474, size: 25, weight: 360, color: "#2B2B2B", maxWidthAbs: 820, lineHeight: 1.45, letterSpacing: 0}
     },
     {
       id:'p6b', title:'Personal Grand Goals',
-      x:62, y:778, w:946, h:387, preset:'BIG', text:'',
-      hdr:{ abs:true, x:183, y:818, size:40, weight:700, color:'#FFFFFF', maxWidthAbs:800 },
-      txt:{ abs:true, x:122, y:897, size:25, weight:400, color:'#1E1E1E', maxWidthAbs:840 }
+      x:61.57, y:778, w:915, h:387, preset:'BIG', text:'',
+      hdr: { abs:true, x:183, y:818, size:40, weight:423, color: "#FFFFFF", maxWidthAbs: 457, align:"center"},
+      txt: { abs:true, x:122, y:897, size:25, weight:400, color: "#2B2B2B", maxWidthAbs: 820, lineHeight: 1.45, letterSpacing: 0}
     },
   ]},
+
   { name:"Cover", bg:FRAME[7], boxes:[] },
 ];
 
@@ -457,6 +469,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
   buildPager();
   renderPage(0);
 });
+
 
 
 
